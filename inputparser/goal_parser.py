99ddb715fa436305f757ca27844dd344328e6134
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2021-06-15 16:30:48 +0200

    fixed goal parser for tail bound goals

--- inputparser/goal_parser.py
+++ inputparser/goal_parser.py
@@ -1,5 +1,6 @@
 from .exceptions import ParseException
 from symengine.lib.symengine_wrapper import sympify
+import re
 
 MOMENT = "MOMENT"
 TAIL_BOUND_UPPER = "TAIL_BOUND_UPPER"
@@ -10,36 +11,30 @@ class GoalParser:
 
     @staticmethod
     def parse(goal: str):
-        # TODO: makes this nice
-        if goal.endswith(" >= ?") or goal.endswith(" <= ?"):
-            goal = goal[0:-5]
-
-        if len(goal) < 3 or goal[1] != "(" or goal[-1] != ")":
+        if len(goal) < 4:
             raise ParseException(f"Malformed goal {goal}")
 
-        between_brackets = goal[2:-1].strip()
         if goal[0] == "E":
-            return GoalParser.__parse_moment__(between_brackets)
+            return GoalParser.__parse_moment__(goal)
         if goal[0] == "P":
-            return GoalParser.__parse_tail_bound__(between_brackets)
+            return GoalParser.__parse_tail_bound__(goal)
         raise ParseException(f"Unknown goal {goal}")
 
     @staticmethod
-    def __parse_moment__(between_brackets: str):
+    def __parse_moment__(goal: str):
+        if goal[1] != "(" or goal[-1] != ")":
+            raise ParseException(f"Malformed goal {goal}")
+        between_brackets = goal[2:-1].strip()
         return MOMENT, [sympify(between_brackets)]
 
     @staticmethod
-    def __parse_tail_bound__(between_brackets: str):
-        if between_brackets.find(">=") >= 0:
-            bound_type = TAIL_BOUND_UPPER
-            split_str = ">="
-        elif between_brackets.find(">") >= 0:
-            bound_type = TAIL_BOUND_LOWER
-            split_str = ">"
-        else:
-            raise ParseException(f"Error in tail bound goal {between_brackets}")
-        tokens = between_brackets.split(split_str)
-        if len(tokens) != 2:
-            raise ParseException(f"Error in tail bound goal {between_brackets}")
-        tokens = [sympify(t.strip()) for t in tokens]
-        return bound_type, tokens
+    def __parse_tail_bound__(goal: str):
+        match = re.search("^P\\(([0-9a-zA-Z*/.+\\-)( ]*?)>=([0-9a-zA-Z*/.+\\-)( ]*?)\\)( )*<=( )*\\?( )*", goal)
+        if match:
+            return TAIL_BOUND_UPPER, [sympify(t) for t in [match.group(1), match.group(2)]]
+
+        match = re.search("^P\\(([0-9a-zA-Z*/.+\\-)( ]*?)>([0-9a-zA-Z*/.+\\-)( ]*?)\\)( )*>=( )*\\?( )*", goal)
+        if match:
+            return TAIL_BOUND_LOWER, [sympify(t) for t in [match.group(1), match.group(2)]]
+
+        raise ParseException(f"Unknown goal {goal}")
