4e2fb9d9a9206a0be1f5f8c412447945fe0fa0c2
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2021-06-23 16:46:42 +0200

    added hermann distribution scripts

---
+++ scripts/hermann3_distribution.py
@@ -0,0 +1,22 @@
+from sympy import *
+from sympy.solvers.solveset import linsolve
+
+n = Symbol("n", integer=True, positive=True)
+p0 = Symbol("p0", real=True)
+p1 = Symbol("p1", real=True)
+p2 = Symbol("p2", real=True)
+p3 = Symbol("p3", real=True)
+
+m1 = 1 + 2*3**(-n)
+m2 = 1 + 8*3**(-n)
+m3 = 1 + 26*3**(-n)
+
+equations = [
+    p0 + p1 + p2 + p3 - 1,
+    0*p0 + 1*p1 + 2*p2 + 3*p3 - m1,
+    (0**2)*p0 + (1**2)*p1 + (2**2)*p2 + (3**2)*p3 - m2,
+    (0**3)*p0 + (1**3)*p1 + (2**3)*p2 + (3**3)*p3 - m3
+]
+
+solution = linsolve(equations, (p0, p1, p2, p3))
+print(solution)
