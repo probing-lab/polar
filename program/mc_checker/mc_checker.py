281ed4b7f747eebf38b92518edf1b2d00d5b7e43
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-08-02 09:42:20 +0200

    Extracted terms in polynomials

--- program/mc_checker/mc_checker.py
+++ program/mc_checker/mc_checker.py
@@ -1,7 +1,9 @@
 from typing import Set, Tuple
 from symengine.lib.symengine_wrapper import Symbol
 from program import Program
-
+from program.assignment import PolyAssignment
+from program.assignment import DistAssignment
+from sympy import Add
 
 GoodVars = Set[Symbol]
 BadVars = Set[Symbol]
@@ -12,4 +14,16 @@ class MCChecker:
     @classmethod
     def get_mc_variables(cls, program: Program) -> Tuple[GoodVars, BadVars]:
         # TODO implement
+
+        print("##########ASSIGNMENTS:############")
+        for assign in program.loop_body:
+            if (isinstance(assign, PolyAssignment)):
+                print("Current assignment: {}".format(assign))
+                for poly in assign.polynomials:
+                    print("Terms of poly {} are {}".format(poly, poly.args))
+
+                print()
+            elif (isinstance(assign, DistAssignment)):
+                print("DIST")
+
         return set(), set()
