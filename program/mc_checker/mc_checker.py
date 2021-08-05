4e64f9b6244a2035d37ce6a7aeba5946e60cc1c3
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-08-05 11:19:24 +0200

    Fix variable extraction from monomials

--- program/mc_checker/mc_checker.py
+++ program/mc_checker/mc_checker.py
@@ -1,75 +1,47 @@
 from typing import Set, Tuple
+
 from symengine.lib.symengine_wrapper import Symbol
 from program import Program
 from program.assignment import PolyAssignment
 from program.assignment import DistAssignment
 from utils import graph
+from utils import expressions
+
 
 GoodVars = Set[Symbol]
 BadVars = Set[Symbol]
 
 
 class MCChecker:
-    @classmethod
-    def get_term_vars(cls, term):
-        for i in range(len(term) - 1):
-            if term[i] == "*" and term[i + 1] == "*":
-                term = term[:i] + "^^" + term[i + 2:]
-        m = term.split("*")
-
-        result = []  # eliminate constant numbers and symbolic variables
-        for item in m:
-            if '0' <= item[0] <= '9':
-                continue
-            result.append(item)
-        # TODO remove symbolic variables from result
-        return result
-
-    @classmethod
-    def clean_var(cls, x):  # remove power and negative sign
-        if "^^" in x:
-            x = x[:x.find("^^")]
-        if x[0] == '-':
-            x = x[1:]
-        return x
 
     @classmethod
     def get_mc_variables(cls, program: Program) -> Tuple[GoodVars, BadVars]:
         # TODO implement
+
+        print("##########PROGRAM VARIABLES##########")
+        print(program.variables)
+        vars_to_index = {var: i for i, var in enumerate(program.variables)}
+        print(vars_to_index)
+
         dependency_graph = graph.Graph()
         for assign in program.loop_body:
             print("Adding node {}".format(str(assign.variable)))
             dependency_graph.add_node(str(assign.variable))
+        print()
 
         print("##########ASSIGNMENTS:############")
         for assign in program.loop_body:
             if isinstance(assign, PolyAssignment):
                 print("Current assignment: {}".format(assign))
-                print()
-
                 assign_var = str(assign.variable)
                 for poly in assign.polynomials:
-                    print("Terms of polynomial {} are {}".format(poly, poly.args))
-                    for term in poly.args:
-                        term_vars = MCChecker.get_term_vars(str(term))
-                        print("Term {} splits into {}".format(term, term_vars))
+                    # TODO: handle dependencies in assignments including if-statements
+                    monoms = expressions.get_terms_with_vars(poly, program.variables)
+                    print("Poly: {} , Monomials: {}".format(poly, monoms))
 
-                        if len(term_vars) == 1:
-                            d_var = MCChecker.clean_var(term_vars[0])
-                            if "^^" in term_vars[0]:
-                                dependency_graph.add_edge(assign_var, d_var, 2)  # non-linear dependency
-                            else:
-                                dependency_graph.add_edge(assign_var, d_var, 1)  # linear dependency
-                        else:
-                            for v in term_vars:
-                                d_var = MCChecker.clean_var(v)
-                                dependency_graph.add_edge(assign_var, d_var, 2)
-
-                    print()
-
-                print()
-                print()
             elif isinstance(assign, DistAssignment):
-                print("DIST")
+                print("Current assignment: {}".format(assign))
+
+            print()
 
         return set(), set()
