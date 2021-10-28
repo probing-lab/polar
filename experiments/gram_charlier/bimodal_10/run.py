b72254e1a8354e6a10135cd3990b8edfda02559e
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2021-10-28 11:02:24 +0200

    changed gram-charlier experiments

--- experiments/gram_charlier/bimodal_10/run.py
+++ experiments/gram_charlier/bimodal_10/run.py
@@ -36,18 +36,20 @@ def main():
 
 def multi_plot():
     sim_data = read_data("simulation.json")
-    gc_fun_8 = read_function(f"gram_charlier_8.txt")
-    gc_fun_18 = read_function(f"gram_charlier_18.txt")
+    gc_fun_6 = read_function(f"gram_charlier_6.txt")
+    gc_fun_12 = read_function(f"gram_charlier_12.txt")
     xs = np.linspace(-20, 20, 100)
-    gc_fun_data_8 = [float(gc_fun_8.subs({Symbol("x"): x})) for x in xs]
-    gc_fun_data_18 = [float(gc_fun_18.subs({Symbol("x"): x})) for x in xs]
-    fig, ax = plt.subplots()
-    ax.hist(sim_data, density=True, bins=100, color="darkgrey", label="Samples")
-    ax.plot(xs, gc_fun_data_18, color="darkblue", linewidth=2, label="GC Order 8")
-    ax.plot(xs, gc_fun_data_8, color="deeppink", linewidth=2, linestyle="dashdot", label="GC Order 18")
+    gc_fun_data_6 = [max(0, float(gc_fun_6.subs({Symbol("x"): x}))) for x in xs]
+    gc_fun_data_12 = [max(0, float(gc_fun_12.subs({Symbol("x"): x}))) for x in xs]
+    fig = plt.figure(figsize=(6, 3))
+    ax = fig.add_subplot()
+    ax.hist(sim_data, density=True, bins=2000, color="darkgrey", label="Samples")
+    ax.plot(xs, gc_fun_data_12, color="darkblue", linewidth=2, label="GC Order 12")
+    ax.plot(xs, gc_fun_data_6, color="red", linewidth=2, linestyle="dashdot", label="GC Order 6")
     ax.yaxis.set_major_formatter(plt.FormatStrFormatter('%.2f'))
-    ax.legend()
-    plt.savefig("gc_plot.pdf")
+    ax.legend(loc="upper right")
+    #plt.show()
+    plt.savefig("gc_plot_10.pdf")
 
 
 if __name__ == "__main__":
