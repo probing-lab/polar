1a44fc8cb3e4f53473c08098178ab30382303219
Julian <e11809619@student.tuwien.ac.at>
2021-11-15 11:33:53 +0100

    partially implemented codegen, added positive testcases

--- bayesnet/parser.py
+++ bayesnet/parser.py
@@ -1,5 +1,6 @@
 import os
 from lark import Lark
+from code_generator import CodeGenerator
 from transformer import NetworkTransformer
 
 GRAMMAR_FILE_PATH = os.path.dirname(os.path.abspath(__file__)) + "/bif-syntax.lark"
@@ -8,15 +9,28 @@ BIF_REPO = os.path.dirname(os.path.abspath(__file__)) + "/repo"
 
 class BifParser:
     """
-    Parsers which takes a .bif source file and returns the program in a form such that it can be used further.
+    Parsers which takes a .bif source file and returns the program into internal representation."
+    Note that this parser is stricter than the "specification" at
+    http://sites.poli.usp.br/p/fabio.cozman/Research/InterchangeFormat/xmlbif02.html
+        1. Sum of a CPT row must lie in the interval (1-cpt_tolerance, 1+cpt_tolerance)
+        2. There is only one table/default entry per CPT allowed.
+        3. The table attribute must contain all CPT entries (no zero padding is done).
+        4. Non-standard blocks (i.e. all except network, variable, probability) are not allowed.
     """
+
+    def __init__(self, cpt_tolerance: float = 0.001):
+        self.cpt_tolerance = cpt_tolerance
 
     def parse_file(self, filepath: str):
         with open(filepath) as file:
             with open(GRAMMAR_FILE_PATH) as grammar_file:
-                parser = Lark(grammar_file, transformer=NetworkTransformer(), parser="lalr")
+                parser = Lark(grammar_file, transformer=NetworkTransformer(self.cpt_tolerance), parser="lalr")
                 network = parser.parse(file.read())
-                print(network.print_pretty())
+        return network
 
 
-BifParser().parse_file(BIF_REPO + "/testcases/asia.bif")
+if "__main__" == __name__:
+    network = BifParser().parse_file(BIF_REPO + "/medium/insurance.bif")
+    print(network.print_pretty())
+    gen = CodeGenerator(network)
+    print(gen.generate_code())
---
+++ bayesnet/tests/test_parse_positive.py
@@ -0,0 +1,28 @@
+import glob
+import os
+import unittest
+
+from parser import BifParser
+
+POSITIVES_PATH = os.path.dirname(os.path.abspath(__file__)) + "/positive"
+
+
+def create_parse_test(file: str):
+    def test(self: ParseFileTest):
+        network = BifParser().parse_file(file)
+        self.assertIsNotNone(network)
+    return test
+
+
+class ParseFileTest(unittest.TestCase):
+    """Checks if parser can parse the given file without errors."""
+    pass
+
+
+positives = glob.glob(POSITIVES_PATH + "/*")
+for positive in positives:
+    name = "test_" + os.path.basename(positive).replace(".bif", "")
+    setattr(ParseFileTest, name, create_parse_test(positive))
+
+if __name__ == '__main__':
+    unittest.main()
