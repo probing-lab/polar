3672a6e0fb662b8fe4e88e67a235d7ce441e3ba5
Julian <e11809619@student.tuwien.ac.at>
2022-04-13 08:18:05 +0200

    [sens] refactored goals_action to make it possible to use its functionality in a flexible way

--- cli/actions/goals_action.py
+++ cli/actions/goals_action.py
@@ -26,30 +26,45 @@ class GoalsAction(Action):
     def __call__(self, *args, **kwargs):
         benchmark = args[0]
         program = parse_program(benchmark, self.cli_args.transform_categoricals)
-        self.program = prepare_program(program, self.cli_args)
+        program = prepare_program(program, self.cli_args)
+        self.initialize_program(program)
+        self.handle_all_goals()
+
+    def initialize_program(self, program: Program):
+        self.program = program
         self.rec_builder = RecBuilder(self.program)
         self.solvers = {}
 
+    def parse_goals(self):
+        if self.cli_args.invariants and not self.cli_args.goals:
+            self.cli_args.goals = [f"E({v})" for v in self.program.original_variables]
+
+        goals = []
+        for goal in self.cli_args.goals:
+            goals.append(GoalParser.parse(goal))
+
+        return goals
+
+    def handle_all_goals(self):
         print(colored("-------------------", "cyan"))
         print(colored("- Analysis Result -", "cyan"))
         print(colored("-------------------", "cyan"))
         print()
 
-        if self.cli_args.invariants and not self.cli_args.goals:
-            self.cli_args.goals = [f"E({v})" for v in self.program.original_variables]
-
         invariant_data = []
-        for goal in self.cli_args.goals:
-            goal_type, goal_data = GoalParser.parse(goal)
+        for goal_type, goal_data in self.parse_goals():
             if goal_type == MOMENT:
-                r = self.handle_moment_goal(goal_data)
-                invariant_data.append((f"E({goal_data[0]})", r))
+                moment, is_exact = self.handle_moment_goal(goal_data)
+                self.print_moment_goal(goal_data[0], moment, is_exact)
+                invariant_data.append((f"E({goal_data[0]})", moment))
             elif goal_type == CUMULANT:
-                r = self.handle_cumulant_goal(goal_data)
-                invariant_data.append((f"k{goal_data[0]}({goal_data[1]})", r))
+                cumulant, is_exact = self.handle_cumulant_goal(goal_data)
+                self.print_cumulant_goal(goal_data[0], goal_data[1], cumulant, is_exact)
+                invariant_data.append((f"k{goal_data[0]}({goal_data[1]})", cumulant))
             elif goal_type == CENTRAL:
-                r = self.handle_central_moment_goal(goal_data)
-                invariant_data.append((f"c{goal_data[0]}({goal_data[1]})", r))
+                cmoment, is_exact = self.handle_central_moment_goal(goal_data)
+                self.print_central_moment_goal(goal_data[0], goal_data[1], cmoment, is_exact)
+                invariant_data.append((f"c{goal_data[0]}({goal_data[1]})", cmoment))
             elif goal_type == TAIL_BOUND_UPPER:
                 self.handle_tail_bound_upper_goal(goal_data)
             elif goal_type == TAIL_BOUND_LOWER:
@@ -68,13 +83,15 @@ class GoalsAction(Action):
             moment = transform_to_after_loop(moment_given_termination)
         else:
             moment, is_exact = get_moment(monom, self.solvers, self.rec_builder, self.cli_args, self.program)
-        print(f"E({monom}) = {prettify_piecewise(moment)}")
+        return moment, is_exact
+
+    def print_moment_goal(self, monom, moment, is_exact, prefix=""):
+        print(f"{prefix}E({monom}) = {prettify_piecewise(moment)}")
         print_is_exact(is_exact)
         if self.cli_args.at_n >= 0:
             moment_at_n = eval_re(self.cli_args.at_n, moment).expand()
-            print(f"E({monom} | n={self.cli_args.at_n}) = {moment_at_n} ≅ {N(moment_at_n)}")
+            print(f"{prefix}E({monom} | n={self.cli_args.at_n}) = {moment_at_n} ≅ {N(moment_at_n)}")
         print()
-        return moment
 
     def handle_cumulant_goal(self, goal_data):
         number = goal_data[0]
@@ -89,13 +106,15 @@ class GoalsAction(Action):
         cumulant = cumulants[number]
         if self.cli_args.after_loop:
             cumulant = transform_to_after_loop(cumulant)
-        print(f"k{number}({monom}) = {prettify_piecewise(cumulant)}")
+        return cumulant, is_exact
+
+    def print_cumulant_goal(self, number, monom, cumulant, is_exact, prefix=""):
+        print(f"{prefix}k{number}({monom}) = {prettify_piecewise(cumulant)}")
         print_is_exact(is_exact)
         if self.cli_args.at_n >= 0:
             cumulant_at_n = eval_re(self.cli_args.at_n, cumulant).expand()
-            print(f"k{number}({monom} | n={self.cli_args.at_n}) = {cumulant_at_n} ≅ {N(cumulant_at_n)}")
+            print(f"{prefix}k{number}({monom} | n={self.cli_args.at_n}) = {cumulant_at_n} ≅ {N(cumulant_at_n)}")
         print()
-        return cumulant
 
     def handle_central_moment_goal(self, goal_data):
         number = goal_data[0]
@@ -110,13 +129,15 @@ class GoalsAction(Action):
         central_moment = central_moments[number]
         if self.cli_args.after_loop:
             central_moment = transform_to_after_loop(central_moment)
-        print(f"c{number}({monom}) = {prettify_piecewise(central_moment)}")
+        return central_moment, is_exact
+
+    def print_central_moment_goal(self, number, monom, central_moment, is_exact, prefix=""):
+        print(f"{prefix}c{number}({monom}) = {prettify_piecewise(central_moment)}")
         print_is_exact(is_exact)
         if self.cli_args.at_n >= 0:
-            central_at_n = eval_re(self.cli_args.at_n, central_moments).expand()
-            print(f"c{number}({monom} | n={self.cli_args.at_n}) = {central_at_n} ≅ {N(central_at_n)}")
+            central_at_n = eval_re(self.cli_args.at_n, central_moment).expand()
+            print(f"{prefix}c{number}({monom} | n={self.cli_args.at_n}) = {central_at_n} ≅ {N(central_at_n)}")
         print()
-        return central_moment
 
     def handle_tail_bound_upper_goal(self, goal_data):
         monom, a = goal_data[0], goal_data[1]
