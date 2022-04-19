07ccf3b05a12a50a432d8da569b1897aa660ef00
Julian <e11809619@student.tuwien.ac.at>
2022-04-19 13:07:02 +0200

    [sens] implemented sensitivity action

--- utils/strings.py
+++ utils/strings.py
@@ -1,5 +1,10 @@
+import string, random
 
 def indent_string(string: str, indent: int):
     lines = string.split("\n")
     lines = [(" " * indent) + line for line in lines]
     return "\n".join(lines)
+
+def random_string(length):
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for i in range(length))
