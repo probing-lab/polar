25e6e21369de75ab9eb830cc6184d53dc5a64d69
Julian <e11809619@student.tuwien.ac.at>
2022-04-22 14:24:02 +0200

    [sens] print system size when solving for a monomial

--- cli/common.py
+++ cli/common.py
@@ -22,6 +22,7 @@ def get_moment(monom, solvers, rec_builder, cli_args, program):
         s = RecurrenceSolver(recurrences, cli_args.numeric_roots, cli_args.numeric_croots, cli_args.numeric_eps)
         solvers.update({sympify(m): s for m in recurrences.monomials})
 
+    print(f"Number of recurrences is {len(recurrences.recurrence_dict)}")
     moment, is_exact = rec_builder.get_solution(monom, solvers)
     return moment, is_exact
 
