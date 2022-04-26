efb60c84570549deb3d66de1b4afe548bfed7322
Julian <e11809619@student.tuwien.ac.at>
2022-04-26 19:48:30 +0200

    [sens] improved benchmark runner, move printing of recurrences

--- benchmarks/sensitivity/some_defective/benchmark.py
+++ benchmarks/sensitivity/some_defective/benchmark.py
@@ -15,6 +15,8 @@ POLAR_DIR = "../../../"
 # benchmarks are assumed to be in the same dir as the script
 BENCHMARK_FOLDER = os.getcwd()
 
+TIMEOUT_SECS = 120
+
 @dataclass
 class Benchmark:
     filename: str
@@ -23,6 +25,7 @@ class Benchmark:
     results: List[str] = None
     duration_sec: float = 0.0
     system_size: int = 0
+    successful: bool = False
 
 # Add, alter or remove benchmarks here in the format "filename", [goals], "parameter"
 benchmarks = [
@@ -30,15 +33,33 @@ benchmarks = [
     Benchmark("diff_effective_2.prob", ["E(u)"], "p"), 
     Benchmark("diff_effective_3.prob", ["E(total)"], "p"), 
     Benchmark("diff_effective_4.prob", ["E(z)"], "p1"),
+    Benchmark("../all_effective/bimodal_x.prob", ["E(x)"], "var"),
+    Benchmark("../all_effective/bimodal_x.prob", ["E(x**2)"], "var"),
+    Benchmark("../all_effective/dbn_component_health.prob", ["E(obs)"], "p1"),
+    Benchmark("../all_effective/dbn_component_health.prob", ["E(obs**2)"], "p1"),
+    Benchmark("../all_effective/gambling.prob", ["E(money)"], "p"),
+    Benchmark("../all_effective/gambling.prob", ["E(money**2)"], "p"),
+    Benchmark("../all_effective/las_vegas_search.prob", ["E(attempts)"], "p"),
+    Benchmark("../all_effective/las_vegas_search.prob", ["E(attempts**2)"], "p"),
+    Benchmark("../all_effective/randomized_response.prob", ["E(p1)"], "p"),
+    Benchmark("../all_effective/randomized_response.prob", ["E(p1**2)"], "p"),
+    Benchmark("../all_effective/vaccination_succinct.prob", ["E(infected)"], "vax_param"),
+    Benchmark("../all_effective/vaccination_succinct.prob", ["E(infected**2)"], "vax_param"),
     ]
     
 def run_benchmark(benchmark: Benchmark, output):
     print(f"Running benchmark {benchmark.filename}...")
     str_goals = "\"" + "\" \"".join(benchmark.goals) + "\""
     cmd = f"python3 polar.py {os.path.join(BENCHMARK_FOLDER, benchmark.filename)} --goals {str_goals} --{SENSITIVITY_TYPE} {benchmark.param}"
-    result = subprocess.check_output(cmd, shell=True).decode() # need to use shell=True so the goals quotation marks dont get escaped
-    output.write(cmd)
-    output.write(result)
+    try:
+        result = subprocess.check_output(cmd, shell=True, timeout=TIMEOUT_SECS).decode() # need to use shell=True so the goals quotation marks dont get escaped
+        output.write(cmd)
+        output.write(result)
+        benchmark.successful = True
+    except subprocess.TimeoutExpired:
+        output.write(cmd)
+        output.write("TIMEOUT")
+        return
     
     # parse runtime from polar output
     time_pattern = "Elapsed time:"
@@ -73,6 +94,10 @@ def run_benchmark(benchmark: Benchmark, output):
         
     
 def print_benchmark(benchmark: Benchmark):
+    if benchmark.successful is False:
+        print(f"Benchmark {benchmark.filename} could not be solved.")
+        return 
+    
     print(f"Benchmark {benchmark.filename} using {benchmark.system_size} recurrences solved in {benchmark.duration_sec} seconds.")
     for i in range(len(benchmark.goals)):
         print(f"\t∂{benchmark.goals[i]} wrt {benchmark.param}: {benchmark.results[i]}")
