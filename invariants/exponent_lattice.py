d96fff1e3527ad6db46420ee84984bd53a6b39b4
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2023-04-07 16:48:31 +0200

    implemented faccin bound

--- invariants/exponent_lattice.py
+++ invariants/exponent_lattice.py
@@ -1,5 +1,6 @@
 from typing import List
-from sympy import Expr, numer, denom
+from sympy import Expr, numer, denom, AlgebraicNumber
+from utils import faccin_bound
 
 from invariants.exceptions import ExponentLatticeException
 from utils import are_coprime
@@ -17,15 +18,20 @@ class ExponentLattice:
     def __init__(self, bases: List[ExponentBase]):
         self.bases = bases
 
-    def compute_basis(self) -> List[List[int]]:
-        if len(self.bases) <= 1:
-            return []
+    def is_trivially_empty(self):
+        if len(self.bases) == 0:
+            return True
         all_rational = all([b.is_Rational for b in self.bases])
         if all_rational:
-            integers = [numer(b) for b in self.bases if numer(b) != 1]
-            integers += [denom(b) for b in self.bases if denom(b) != 1]
-            if are_coprime(integers):
-                return []
+            numerators = [numer(b) for b in self.bases if numer(b) != 1]
+            denominators = [denom(b) for b in self.bases if denom(b) != 1]
+            if all([abs(n) > 1 for n in numerators]) and are_coprime(numerators + denominators):
+                return True
+        return False
+
+    def compute_basis_sage(self) -> List[List[int]]:
+        if self.is_trivially_empty():
+            return []
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
         command = ["sage", dir_path + "/integer-relations.py"] + [str(b) for b in self.bases]
@@ -36,3 +42,10 @@ class ExponentLattice:
             return json.loads(result.stdout)
         except FileNotFoundError:
             raise ExponentLatticeException("To compute the exponent lattice for invariants please install sagemath and make the 'sage' command visible on your system.")
+
+    def compute_basis(self) -> List[List[int]]:
+        if self.is_trivially_empty():
+            return []
+
+        alg_bases = [AlgebraicNumber(b) for b in self.bases]
+        print(faccin_bound(alg_bases))
---
+++ tests/test_exponent_lattice.py
@@ -0,0 +1,44 @@
+import unittest
+from sympy import sympify
+from invariants.exponent_lattice import ExponentLattice
+
+
+class ExponentLatticeTest(unittest.TestCase):
+
+    def test_basis_1(self):
+        n1 = sympify("2")
+        n2 = sympify("1/2")
+        lattice = ExponentLattice([n1, n2])
+        basis = lattice.compute_basis()
+        self.assertEqual(basis, [[1, 1]])
+
+    def test_basis_2(self):
+        n1 = sympify("1")
+        n2 = sympify("-1")
+        lattice = ExponentLattice([n1, n2])
+        basis = lattice.compute_basis()
+        self.assertEqual(basis, [[1, 0], [0, 2]])
+
+    def test_basis_3(self):
+        n1 = sympify("CRootOf('x**2 + 1', 0)")
+        n2 = sympify("CRootOf('x**2 + 1', 1)")
+        lattice = ExponentLattice([n1, n2])
+        basis = lattice.compute_basis()
+        self.assertEqual(basis, [[1, 1], [2, -2]])
+
+    def test_basis_4(self):
+        n1 = sympify("2")
+        n2 = sympify("1/2")
+        n3 = sympify("1")
+        n4 = sympify("-1")
+        n5 = sympify("CRootOf('x**2 + 1', 0)")
+        n6 = sympify("CRootOf('x**2 + 1', 1)")
+        lattice = ExponentLattice([n1, n2, n3, n4, n5, n6])
+        basis = lattice.compute_basis()
+        self.assertEqual(basis, [[1, 1, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 2], [0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 0, 4]])
+
+    def test_tmp(self):
+        n1 = sympify("sqrt(2)")
+        n2 = sympify("2")
+        lattice = ExponentLattice([n1, n2])
+        basis = lattice.compute_basis()
