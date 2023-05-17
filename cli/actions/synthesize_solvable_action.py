8130df416696e33dead6bdd8c3eb76c48886c522
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2023-05-17 15:39:28 +0200

    applied comments of pull-request on solvable loop synthesis

--- cli/actions/synthesize_solvable_action.py
+++ cli/actions/synthesize_solvable_action.py
@@ -1,10 +1,9 @@
 from argparse import Namespace
+from symengine.lib.symengine_wrapper import sympify
 from .action import Action
 from cli.common import prepare_program, parse_program
 from termcolor import colored
 from program.solvable_synthesizer import SolvableSynthesizer
-from program import Program
-from program.assignment import PolyAssignment
 
 
 class SynthSolvAction(Action):
@@ -15,7 +14,7 @@ class SynthSolvAction(Action):
 
     def __call__(self, *args, **kwargs):
         benchmark = args[0]
-        combination_deg = self.cli_args.synth_solv
+        combination_deg = self.cli_args.mc_comb_deg
         program = parse_program(benchmark, self.cli_args.transform_categoricals)
         program = prepare_program(program, self.cli_args)
 
@@ -24,39 +23,36 @@ class SynthSolvAction(Action):
             return
 
         combination_vars = []
-        for var in program.non_mc_variables:
-            if var in program.original_variables:
-                combination_vars.append(var)
+        if len(self.cli_args.synth_solv) == 0:
+            for var in program.non_mc_variables:
+                if var in program.original_variables:
+                    combination_vars.append(var)
+        else:
+            combination_vars = [sympify(v) for v in self.cli_args.synth_solv]
 
         print(colored("-------------------", "cyan"))
         print(colored("- Analysis Result -", "cyan"))
         print(colored("-------------------", "cyan"))
         print()
+        invariants, solvable_programs = SolvableSynthesizer.synthesize(
+            combination_vars,
+            combination_deg,
+            program,
+            self.cli_args.numeric_roots,
+            self.cli_args.numeric_croots,
+            self.cli_args.numeric_eps
+        )
 
-        solvable_programs = []
-        combinations = SolvableSynthesizer.synthesize(combination_vars, combination_deg, program)
-        if combinations is None:
-            print(f"No combination found with degree {combination_deg}. Try using higher degrees.")
-            return program
-        else:
-            for combination in combinations:
-                solvable_loop_body = []
-                solvable_loop_variables = []
-                for poly_assign in program.loop_body:
-                    var = poly_assign.variable
-                    if var in program.mc_variables:
-                        solvable_loop_body.append(poly_assign)
-                        solvable_loop_variables.append(var)
-                comb_var, comb_var_assign = combination[0], combination[1]
-                solvable_loop_body.append(PolyAssignment(comb_var, [comb_var_assign], [1]))
-                solvable_loop_variables.append(comb_var)
+        if len(invariants) == 0:
+            print(f"No solvable loop found with degree {combination_deg}. Try using other degrees.")
+            return None, None
 
-                solvable_program = Program([], solvable_loop_variables, solvable_loop_variables, program.initial,
-                                   program.loop_guard, solvable_loop_body, program.is_probabilistic)
-                solvable_program.typedefs = program.typedefs
-
-                print("Synthesized solvable loop: ")
-                print(solvable_program)
-                print()
-                solvable_programs.append(solvable_program)
-        return solvable_programs
+        for (invariant, solvable_program) in zip(invariants, solvable_programs):
+            print("Synthesized solvable loop: ")
+            print(solvable_program)
+            print()
+            print("Invariant used: ")
+            print(invariant)
+            print()
+            print()
+        return invariants, solvable_programs
--- program/solvable_synthesizer/solvable_synthesizer.py
+++ program/solvable_synthesizer/solvable_synthesizer.py
@@ -1,209 +1,25 @@
-from utils import get_unique_var, solve_rec_by_summing, get_terms_with_vars, get_monoms
-from symengine.lib.symengine_wrapper import Symbol
-from sympy import solve, sympify, groebner
+from program.assignment import PolyAssignment
+from program.mc_comb_finder import MCCombFinder
 from recurrences import RecBuilder
+from utils import get_unique_var
+from symengine.lib.symengine_wrapper import Symbol
 from program import Program
-from recurrences.solver import RecurrenceSolver
-
 
 class SolvableSynthesizer:
     """
-    Synthesizes all solvable loops from an unsolvable loop
+    Synthesizes all solvable loops from an unsolvable loop and returns a list of pairs (inv, program) where inv is the
+    list of invariants used for replacing the defective variables with a fresh variable and program is the
+    corresponding solvable loop.
     """
-
     @classmethod
-    def __get_candidate_terms__(cls, pos, vars, deg, pw, s):
-        if pos == len(vars):
-            if s == 0:
-                return 0, {0}
-            term = 1
-            for i in range(pos):
-                term = term * (vars[i] ** pw[i])
-            coeff = Symbol(get_unique_var())
-            term = coeff * term
-            return term, {coeff}
-        ans = 0
-        coefficients = set()
-        for i in range(deg + 1):
-            if s + i <= deg:
-                pw[pos] = i
-                expr, c = cls.__get_candidate_terms__(pos + 1, vars, deg, pw, s + i)
-                if expr == 0:
-                    continue
-                ans += expr
-                for elem in c:
-                    coefficients.add(elem)
-        return ans, coefficients,
-
-    @classmethod
-    def __get_candidate__(cls, vars, deg):
-        ans, coeffs = cls.__get_candidate_terms__(0, vars, deg, [0] * len(vars), 0)
-        return ans, coeffs
-
-    @classmethod
-    def __get_good_set__(cls, candidate_rec, bad_variables, variables):
-        """
-        Get set of all effective monomials appearing in candidate_rec
-        """
-        result = set()
-        monoms = get_terms_with_vars(candidate_rec, variables)[0]
-        index_to_vars = {i: var for i, var in enumerate(variables)}
-        for monom, coeff in monoms:
-            bad = False
-            term = 0
-            for i in range(len(monom)):
-                if monom[i] == 0:
-                    continue
-                cur_var = index_to_vars[i]
-                if cur_var in bad_variables:
-                    bad = True
-                term += cur_var ** monom[i]
-            if not bad and sum(monom) > 0:
-                result.add(term)
-        return result
-
-    @classmethod
-    def __get_good_poly__(cls, good_set):
-        """
-        Construct the polynomial representing the sigma on rhs of Eq. (3) in paper
-        """
-        rhs_good_part = 0
-        symbols = set()
-        for term in good_set:
-            coeff = Symbol(get_unique_var())
-            symbols.add(coeff)
-            rhs_good_part += coeff * term
-
-        coeff = Symbol(get_unique_var())
-        symbols.add(coeff)
-        rhs_good_part += coeff
-
-        return rhs_good_part, symbols
-
-    @classmethod
-    def __solution_exact__(cls, equations, solution):
-        nequations = []
-        nequations_variables = set()
-        okay = True
-        for eq in equations:
-            substituted_equation = eq.subs(solution).simplify()
-            if not substituted_equation.is_number:
-                okay = False
-                nequations.append(substituted_equation)
-                for symb in substituted_equation.free_symbols:
-                    nequations_variables.add(symb)
-        return okay, nequations, nequations_variables
-
-    @classmethod
-    def __get_init_value_candidate__(cls, candidate, rec_builder):
-        """
-        Computes the initial value of the candidate (0'th iteration, i.e., before loop).
-        """
-        ans = 0
-        monoms = get_monoms(candidate, rec_builder.program.variables)
-        for monom, coeff in monoms:
-            ans += rec_builder.get_initial_value(monom) * coeff
-        return ans
-
-    @classmethod
-    def __construct_equations__(cls, candidate_rec, candidate_coefficients, kcandidate, rhs_good_part, good_coeffs, k):
-        """
-        Constructs system of equations derived from main equation (Eq. (3) in paper) for feeding into SymPy.
-        """
-        candidate_rec_monoms = get_monoms(candidate_rec, candidate_coefficients, with_constant=True)
-        kcandidate_monoms = get_monoms(kcandidate, candidate_coefficients | {k}, with_constant=True)
-        good_part_monoms = get_monoms(rhs_good_part, good_coeffs, with_constant=True)
-        equation_terms = {}
-        for coeff, monom in candidate_rec_monoms:
-            equation_terms[monom] = equation_terms.get(monom, 0) + coeff
-        for coeff, monom in kcandidate_monoms:
-            equation_terms[monom] = equation_terms.get(monom, 0) - coeff
-        for coeff, monom in good_part_monoms:
-            equation_terms[monom] = equation_terms.get(monom, 0) - coeff
-        equations = []
-        for eq in equation_terms.values():
-            equations.append(eq)
-        return equations
-
-    @classmethod
-    def __get_nice_solutions__(cls, solutions, equations, candidate, k):
-        """
-        Keeps plugging in solutions of SymPy and solving new-made equations until all evaluate to zero. Also,
-        eliminates trivial solutions.
-        """
-        nice_solutions = []
-        for solution in solutions:
-            solution_exact, nequations, nequations_variables = cls.__solution_exact__(equations, solution)
-            wrong_solution = False
-            while not solution_exact:
-                nsolutions = solve(nequations, nequations_variables)
-                if type(nsolutions) is list:
-                    if len(nsolutions) == 0:
-                        wrong_solution = True
-                        break
-                else:
-                    for var in solution.keys():
-                        solution[var] = solution[var].subs(nsolutions).simplify()
-                    for var in nsolutions:
-                        solution[var] = nsolutions[var]
-                solution_exact, nequations, nequations_variables = cls.__solution_exact__(equations, solution)
-            if not wrong_solution:
-                nice_solutions.append(solution)
-        solutions = nice_solutions
-        nice_solutions = []
-        for solution in solutions:
-            if candidate.xreplace(solution) == 0:
-                continue
-            if not sympify(k) in solution:
-                continue
-            nice_solutions.append(solution)
-        return nice_solutions
-
-    @classmethod
-    def __solve_good_part__(cls, rhs_good_part, good_coeffs, numeric_roots, numeric_croots, numeric_eps, program):
-        """
-        Fina a closed form for the summation in Eq. (3) with respect to n and initial values.
-        """
-        good_part_monoms = get_monoms(rhs_good_part, good_coeffs, with_constant=True)
-        good_part_solution = 0
-        for coeff, monom in good_part_monoms:
-            if monom == 1:
-                good_part_solution += coeff
-                continue
-            rec_builder = RecBuilder(program)
-            recs = rec_builder.get_recurrences(monom)
-            rec_solver = RecurrenceSolver(
-                recurrences=recs,
-                numeric_roots=numeric_roots,
-                numeric_croots=numeric_croots,
-                numeric_eps=numeric_eps
-            )
-            good_part_solution += coeff * rec_solver.get(monom)
-        good_part_solution = good_part_solution.xreplace({sympify("n"): sympify("n") - 1})
-        return good_part_solution
-
-
-    @classmethod
-    def synthesize(cls, combination_vars, combination_deg, program: Program):
-        candidate, candidate_coefficients = cls.__get_candidate__(combination_vars, combination_deg)
-        rec_builder = RecBuilder(program)
-        candidate_rec = rec_builder.get_recurrence_poly(candidate, combination_vars)
-
-        good_set = cls.__get_good_set__(candidate_rec, program.non_mc_variables, program.variables)
-        rhs_good_part, good_coeffs = cls.__get_good_poly__(good_set)
-        k = Symbol(get_unique_var("k"), nonzero=True)
-        kcandidate = (k * candidate).expand()
-
-        equations = cls.__construct_equations__(
-            candidate_rec, candidate_coefficients, kcandidate, rhs_good_part, good_coeffs, k
-        )
-
-        symbols = list(candidate_coefficients) + list(good_coeffs) + [k]
-        equations = [sympify(e) for e in equations]
-        symbols = [sympify(s) for s in symbols]
-        basis = groebner(equations, *symbols)
-        solutions = solve([b for b in basis], symbols, dict=True)
-        nice_solutions = cls.__get_nice_solutions__(solutions, equations, candidate, k)
+    def synthesize(cls, combination_vars, combination_deg, program: Program, numeric_roots, numeric_croots, numeric_eps):
+        candidate, candidate_rec, candidate_coefficients = MCCombFinder.construct_candidate(combination_vars, combination_deg,
+                                                                                   program)
+        rhs_good_part, good_coeffs = MCCombFinder.construct_inhomogeneous(candidate_rec, program.non_mc_variables,
+                                                                 program.variables)
+        k, kcandidate = MCCombFinder.construct_homogenous(candidate)
+        nice_solutions = MCCombFinder.solve_quadratic_system(candidate, candidate_rec, candidate_coefficients, kcandidate,
+                                                    rhs_good_part, good_coeffs, k)
         if len(nice_solutions) == 0:
             return None
 
@@ -213,6 +29,30 @@ class SolvableSynthesizer:
             ans = (k.xreplace(solution)) * s + (rhs_good_part.xreplace(solution))
             combinations.append((s, ans))
 
-        return combinations
+        if combinations is None:
+            print(f"No combination found with degree {combination_deg}. Try using higher degrees.")
+            return program
+        else:
+            rec_builder = RecBuilder(program)
+            solvable_programs = []
+            for combination in combinations:
+                solvable_loop_body = []
+                solvable_loop_variables = []
+                for poly_assign in program.loop_body:
+                    var = poly_assign.variable
+                    if var in program.mc_variables:
+                        solvable_loop_body.append(PolyAssignment(var, [rec_builder.get_recurrence_poly(var, [var])], [1]))
+                        solvable_loop_variables.append(var)
+                comb_var, comb_var_assign = combination[0], combination[1]
+                solvable_loop_body.append(PolyAssignment(comb_var, [comb_var_assign], [1]))
+                solvable_loop_variables.append(comb_var)
 
+                solvable_program = Program([], solvable_loop_variables, solvable_loop_variables, program.initial,
+                                   program.loop_guard, solvable_loop_body, program.is_probabilistic)
+                solvable_program.typedefs = program.typedefs
 
+                solvable_programs.append(solvable_program)
+
+        invariants = MCCombFinder.get_invariants(candidate, rec_builder, nice_solutions, rhs_good_part, good_coeffs,
+                                                 numeric_roots, numeric_croots, numeric_eps, program, k)
+        return invariants, solvable_programs
