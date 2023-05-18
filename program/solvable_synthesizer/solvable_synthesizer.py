e41bebd4849cb3f4059b623c15e0767b9e97f310
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2023-05-18 09:45:32 +0200

    refactor: broke into smaller functions

--- program/solvable_synthesizer/solvable_synthesizer.py
+++ program/solvable_synthesizer/solvable_synthesizer.py
@@ -14,68 +14,55 @@ class SolvableSynthesizer:
     """
 
     @classmethod
-    def synthesize(
+    def handle_solvable_loop(cls, program):
+        rec_builder = RecBuilder(program)
+        solvable_loop_body = []
+        solvable_loop_variables = []
+        fresh = dict()
+        for poly_assign in program.loop_body:
+            var = poly_assign.variable
+            if var in program.mc_variables:
+                t = Symbol(get_unique_var("t"), nonzero=True)
+                solvable_loop_body.append(
+                    PolyAssignment(
+                        t, [rec_builder.get_recurrence_poly(var, [var])], [1]
+                    )
+                )
+                fresh[var] = t
+                solvable_loop_variables.append(t)
+
+        for poly_assign in program.loop_body:
+            var = poly_assign.variable
+            if var in program.mc_variables:
+                solvable_loop_body.append(PolyAssignment(var, [fresh[var]], [1]))
+
+        return [], [
+            Program(
+                [],
+                solvable_loop_variables,
+                solvable_loop_variables,
+                program.initial,
+                program.loop_guard,
+                solvable_loop_body,
+                program.is_probabilistic,
+            )
+        ]
+
+    @classmethod
+    def handle_unsolvable_loop(
         cls,
+        nice_solutions,
+        program,
+        rhs_good_part,
         combination_vars,
-        combination_deg,
-        program: Program,
+        candidate,
+        good_coeffs,
         numeric_roots,
         numeric_croots,
         numeric_eps,
+        k,
     ):
-        (
-            candidate,
-            candidate_rec,
-            candidate_coefficients,
-        ) = MCCombFinder.construct_candidate(combination_vars, combination_deg, program)
-        rhs_good_part, good_coeffs = MCCombFinder.construct_inhomogeneous(
-            candidate_rec, program.non_mc_variables, program.variables
-        )
-        k, kcandidate = MCCombFinder.construct_homogenous(candidate)
-        nice_solutions = MCCombFinder.solve_quadratic_system(
-            candidate,
-            candidate_rec,
-            candidate_coefficients,
-            kcandidate,
-            rhs_good_part,
-            good_coeffs,
-            k,
-        )
-
         rec_builder = RecBuilder(program)
-        if len(nice_solutions) == 0:  # no combination found
-            solvable_loop_body = []
-            solvable_loop_variables = []
-            fresh = dict()
-            for poly_assign in program.loop_body:
-                var = poly_assign.variable
-                if var in program.mc_variables:
-                    t = Symbol(get_unique_var("t"), nonzero=True)
-                    solvable_loop_body.append(
-                        PolyAssignment(
-                            t, [rec_builder.get_recurrence_poly(var, [var])], [1]
-                        )
-                    )
-                    fresh[var] = t
-                    solvable_loop_variables.append(t)
-
-            for poly_assign in program.loop_body:
-                var = poly_assign.variable
-                if var in program.mc_variables:
-                    solvable_loop_body.append(PolyAssignment(var, [fresh[var]], [1]))
-
-            return [], [
-                Program(
-                    [],
-                    solvable_loop_variables,
-                    solvable_loop_variables,
-                    program.initial,
-                    program.loop_guard,
-                    solvable_loop_body,
-                    program.is_probabilistic,
-                )
-            ]
-
         combinations = []
         for solution in nice_solutions:
             s = Symbol(get_unique_var("s"), nonzero=True)
@@ -137,3 +124,48 @@ class SolvableSynthesizer:
             k,
         )
         return invariants, solvable_programs
+
+    @classmethod
+    def synthesize(
+        cls,
+        combination_vars,
+        combination_deg,
+        program: Program,
+        numeric_roots,
+        numeric_croots,
+        numeric_eps,
+    ):
+        (
+            candidate,
+            candidate_rec,
+            candidate_coefficients,
+        ) = MCCombFinder.construct_candidate(combination_vars, combination_deg, program)
+        rhs_good_part, good_coeffs = MCCombFinder.construct_inhomogeneous(
+            candidate_rec, program.non_mc_variables, program.variables
+        )
+        k, kcandidate = MCCombFinder.construct_homogenous(candidate)
+        nice_solutions = MCCombFinder.solve_quadratic_system(
+            candidate,
+            candidate_rec,
+            candidate_coefficients,
+            kcandidate,
+            rhs_good_part,
+            good_coeffs,
+            k,
+        )
+
+        if len(nice_solutions) == 0:  # no combination found
+            return cls.handle_solvable_loop(program)
+        else:
+            return cls.handle_unsolvable_loop(
+                nice_solutions,
+                program,
+                rhs_good_part,
+                combination_vars,
+                candidate,
+                good_coeffs,
+                numeric_roots,
+                numeric_croots,
+                numeric_eps,
+                k,
+            )
