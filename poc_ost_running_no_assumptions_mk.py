ba37423ff09ff361c897daa501c0c3f828cc0c31
Lorenz Winkler <lorenz.winkler@tuwien.ac.at>
2025-12-01 18:36:05 +0100

    add example with unbounded support affecting loop guard

--- poc_ost_running_no_assumptions_mk.py
+++ poc_ost_running_no_assumptions_mk.py
@@ -1,9 +1,8 @@
 from functools import reduce
 from typing import List, Tuple
-from sympy import Expr, Rational, Symbol, oo, preorder_traversal, solve, sympify
+from sympy import Expr, Symbol, oo, preorder_traversal, solve, sympify
 from extension_ost.bound_computation import compute_bounds
 from extension_ost.bound_store import BoundStore
-from extension_ost.helpers import Expexted
 from extension_ost.saturation.saturation_based_bound_computation import compute_bounds_saturation
 from inputparser.parser import Parser
 from program.condition.true_cond import TrueCond
@@ -12,7 +11,7 @@ from program.transformer import normalize_program
 from recurrences.rec_builder import RecBuilder
 from termination.martingales.branches.branch_builder import BranchBuilder
 
-program = Parser().parse_file("documentation/loops_ost_extension/running_example_unbounded.prob")
+program = Parser().parse_file("documentation/loops_ost_extension/running_example.prob")
 lg = program.loop_guard
 print(f"Loop guard: {lg}")
 if lg.cop != '>':
@@ -79,14 +78,14 @@ compute_bounds_saturation(random_vars,
                deterministic_vars,
                2,
                {Symbol("x", real=True):1,
-                Symbol("z", real=True):1,
+                Symbol("y", real=True):1,
+                Symbol("z", real=True):2,
                 Symbol("k", real=True):1},
                recurrence_builder,
-               [(Symbol("x0", is_finite=True, positive=True),sympify(0), oo)],
-               {Symbol("k", real=True):sympify(1), 
-                Expexted(Symbol("x", real=True)):-sympify(Rational(13,10))},
-               {Symbol("x", real=True): sympify(0),
-                Expexted(Symbol("x", real=True)**2):sympify(Rational(23,10))},
-               use_minkowski=True,
-               num_sparsest_solutions=100,
-               keep_non_optimal_martingales=False)
+               [(Symbol("x0", is_finite=True, positive=True),sympify(0), oo), 
+                (Symbol("y0", is_finite=True),-oo, oo),
+                (Symbol("z0", is_finite=True),-oo, oo)],
+               {Symbol("x", real=True): sympify(-1), Symbol("k", real=True):sympify(1)},
+               {Symbol("x", real=True): sympify(0)},
+               num_sparsest_solutions=20,
+               use_minkowski=True)
